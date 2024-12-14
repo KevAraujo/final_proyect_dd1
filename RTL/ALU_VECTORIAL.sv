0c6434b4c7a68c451ed429ac51a3f02184dc72d5
KevAraujo <kevin.araujo@cinvestav.mx>
2024-12-14 02:05:31 -0600

    Update ALU_VECTORIAL.sv

--- RTL/ALU_VECTORIAL.sv
+++ RTL/ALU_VECTORIAL.sv
@@ -1,4 +1,4 @@
-module ALU_VECTORIAL #(parameter WIDTH = 4, n_alu = 1)(
+module ALU_VECTORIAL #(parameter WIDTH = 8, n_alu = 4)(
     input logic clk,               // Reloj de entrada
     //input logic rst,           // Señal de reset
     input wire [WIDTH*n_alu-1:0] a,    // Entrada de datos
@@ -6,15 +6,17 @@ module ALU_VECTORIAL #(parameter WIDTH = 4, n_alu = 1)(
     input wire [2:0] select,
     input wire arst,
     //input wire enable,
-    output wire carry_out,
+    output wire [n_alu-1:0]carry_out,
     input wire [n_alu-1:0] enable,
-    output wire a_greater, a_equal, a_less, 
-    output wire [n_alu-1:0] inf,
+    output wire [n_alu-1:0] a_greater, a_equal, a_less, inf,
     output wire [WIDTH*n_alu*2-1:0] data_out   // Salida de datos
 );
-
+//logic [WIDTH*n_alu-1:0]a_w;
+//assign a = a_w + 16'h0100;
+    // Declaración de la cantidad de instancias
     genvar i;
 
+    // Generación de instancias del módulo `mi_modulo`
     generate
         for (i = 0; i < n_alu; i = i + 1) begin : gen_inst
             ALU u_modulo (
@@ -22,16 +24,17 @@ module ALU_VECTORIAL #(parameter WIDTH = 4, n_alu = 1)(
                 //.rst(rst),
                 .a(a[(i*WIDTH) +: WIDTH]),
                 .b(b[(i*WIDTH) +: WIDTH]),
-                .out(data_out[i*8 +: 8]),
+                .out(data_out[i*WIDTH*2 +: (WIDTH*2)]),
                 .select(select),
                 .arst(arst),
                 .enable(enable[i]),
-                .carry_out(carry_out), 
-                .a_greater(a_greater),
-                .a_equal(a_equal),
-                .a_less(a_less),
+                .carry_out(carry_out[i]), 
+                .a_greater(a_greater[i]),
+                .a_equal(a_equal[i]),
+                .a_less(a_less[i]),
                 .inf(inf[i])
             );
         end
     endgenerate
+
 endmodule
